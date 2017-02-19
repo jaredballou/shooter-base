2d40ff105cb1e96a9e93942e977250ac44dc0364
Jared Ballou <jballou@jballou.com>
2017-02-18 22:30:24 -0500

    Rebased master without hl2sb crap

---
+++ modtool.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+""" modtool.py
+(c) 2016,2017 Jared Ballou <github@jballou.com>
+
+This is a simple tool to handle building a Source Engine mod based upon a settings file.
+"""
+import os
+import sys
+
+
+
+# Main functions
+## Settings
+## Create Mod
+## Mod Functions
+### Run Hammer
+### Run HLMV
+### Build Mod
+### Run Mod
+### Run StudioMDL
